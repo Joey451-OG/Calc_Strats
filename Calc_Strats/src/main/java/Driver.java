b35be9884e391eaccb553fa2778f04612fbb0e33
ItIsJoeyG <itsjoeygofficial@outlook.com>
2024-03-19 21:17:03 -0500

    Initial Commit

---
+++ Calc_Strats/src/main/java/Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+    public static void main(String[] args) {
+        System.out.print("""
+                So here's the idea:
+                1. Parse the user input using LaTeX
+                2. Each time an expression (integral, fraction, squared variable) is found, the software
+                \twill add 1 to all strategy object that have that property
+                3. The program will end by showing a list of highest rated strategies
+
+                Anyways, the pom.xml file is giving me some weird errors, I'm gonna go play minecraft instead of fixing them.""");
+
+    }
+}
